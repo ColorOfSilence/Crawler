715796f038b3d4589dc3965a1ec5ba0762c83a3f
jdonosa <juandonosa@hotmail.com>
2017-09-02 16:55:38 -0700

    the program seems to be working correctly now, it seperates the data like it should; need to make a JSON file to check

--- webcrawler/webcrawler/spiders/binf.py
+++ webcrawler/webcrawler/spiders/binf.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#start with the 5th element in td to get 'Gene Name'
+#response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[0].extract()
 import scrapy
 
 
@@ -14,22 +14,23 @@ class BinfSpider(scrapy.Spider):
             'http://crdd.osdd.net/raghava/herceptinr/submit_pair_main.php?ran=3102&num=16582&method=exp'
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_data)
 
-    def parse(self, response):
-        for data in response.css('td'):
-            start = 15
+    def parse_data(self, response):
+        start = -1
+        for data in response.xpath('//div[@id="mainHolder"]/table/tr/td'):
             yield {
-                'Gene Name' : response.css('td::text')[(start+=1)].extract(),
-                'SKBR3 Expression' : response.css('td::text').extract(),
-                'HCC1569 Expression' : response.css('td::text').extract(),
-                'SKBR3 Percentile': response.css('td::text').extract(),
-                'HCC1569 Percentile': response.css('td::text').extract(),
-                'D1/D2': response.css('td::text').extract(),
-                'D2/D1': response.css('td::text').extract(),
-                'D1-D2': response.css('td::text').extract(),
-                'D2-D1': response.css('td::text').extract(),
+                'Gene Name' : response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[(start+1)].extract(),
+                'SKBR3 Expression' : response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[start+2].extract(),
+                'HCC1569 Expression' : response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[start+3].extract(),
+                'SKBR3 Percentile': response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[start+4].extract(),
+                'HCC1569 Percentile': response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[start+5].extract(),
+                'D1/D2': response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[start+6].extract(),
+                'D2/D1': response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[start+7].extract(),
+                'D1-D2': response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[start+8].extract(),
+                'D2-D1': response.xpath('//div[@id="mainHolder"]/table/tr/td/text()')[start+9].extract(),
             }
+            start += 9
         # page = response.url.split("/")[-2]
         # filename = 'binf-%s.html' % page
         # with open(filename, 'wb') as f:
